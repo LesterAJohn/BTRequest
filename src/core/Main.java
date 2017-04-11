66506d5e52431e2ee747da599eb8822143bc84c6
LesterAJohn <LesterAJohn@gmail.com>
2017-04-11 16:03:14 -0500

    Updated Comments

--- src/core/Main.java
+++ src/core/Main.java
@@ -5,12 +5,15 @@ public class Main {
 		
 		//Array duplicate removal function; while keeping position consistency
 		//Will remove duplicate int numbers next to each other
+		//Alter array below to adjust output
 		
 		int[] nArray = {1,1,1,2,1,2,3,3,8,9,12,4,5,6,7,7};
 		pArray.arrayValue(nArray);
 		
-		//Mathematically Computes Nodes
+		//Mathematically Computes Nodes with 2 Children Each
 		//Provides Node with Maximum and Minimum Values
+		//Working on in memory and disk storage for node structure
+		//Adjust numbers below to define number of nodes created
 		
 		Node.nodeArray(10);
 	}
