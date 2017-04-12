4bea11d14c1b53689fe0f8afe75614702ad12d16
LesterAJohn <LesterAJohn@gmail.com>
2017-04-11 21:36:59 -0500

    Node Variable Naming

--- src/core/Node.java
+++ src/core/Node.java
@@ -9,55 +9,55 @@ public class Node
 		int[] storeValueMax = {0,0,0,0,0};
 		int[] storeValueMin = {0,0,0,0,0};
 		
-		ArrayList<Integer> nodeE1 = new ArrayList<Integer>();
-		ArrayList<Integer> nodeE2 = new ArrayList<Integer>();
-		ArrayList<Integer> nodeE3 = new ArrayList<Integer>();
-		ArrayList<Integer> nodeE4 = new ArrayList<Integer>();
+		ArrayList<Integer> nodeParent = new ArrayList<Integer>();
+		ArrayList<Integer> nodeC1 = new ArrayList<Integer>();
+		ArrayList<Integer> nodeC2 = new ArrayList<Integer>();
+		ArrayList<Integer> nodeValue = new ArrayList<Integer>();
 		
 		for(int i=0;i<nodeNum;i++)
 		{
 			if((i%2)==0)
 			{
-				//even
-				nodeE1.add(i, i/2);
-				nodeE2.add(i, i*2);
-				nodeE3.add(i, (i*2)+1);
-				nodeE4.add(i, (int)Math.ceil(Math.random()*(100)));
+				//even Nodes Generator
+				nodeParent.add(i, i/2);
+				nodeC1.add(i, i*2);
+				nodeC2.add(i, (i*2)+1);
+				nodeValue.add(i, (int)Math.ceil(Math.random()*(100)));
 			}
 
 			else
 			{
-				//Odd
-				nodeE1.add(i, (i-1)/2);
-				nodeE2.add(i, i*2);
-				nodeE3.add(i, (i*2)+1);
-				nodeE4.add(i, (int)Math.ceil(Math.random()*(100)));
+				//odd Nodes Generator
+				nodeParent.add(i, (i-1)/2);
+				nodeC1.add(i, i*2);
+				nodeC2.add(i, (i*2)+1);
+				nodeValue.add(i, (int)Math.ceil(Math.random()*(100)));
 			}
 		}
 		
-		for(int i=0;i<nodeE4.size();i++)
+		for(int i=0;i<nodeValue.size();i++)
 		{
-			if(nodeE4.get(i) > storeValueMax[4])
+			if(nodeValue.get(i) > storeValueMax[4])
 			{
 				storeValueMax[0] = i;
-				storeValueMax[1] = nodeE1.get(i);
-				storeValueMax[2] = nodeE2.get(i);
-				storeValueMax[3] = nodeE3.get(i);
-				storeValueMax[4] = nodeE4.get(i);
+				storeValueMax[1] = nodeParent.get(i);
+				storeValueMax[2] = nodeC1.get(i);
+				storeValueMax[3] = nodeC2.get(i);
+				storeValueMax[4] = nodeValue.get(i);
 			}
 			
-			if(nodeE4.get(i) < storeValueMin[4] | storeValueMin[4] == 0)
+			if(nodeValue.get(i) < storeValueMin[4] | storeValueMin[4] == 0)
 			{
 				storeValueMin[0] = i;
-				storeValueMin[1] = nodeE1.get(i);
-				storeValueMin[2] = nodeE2.get(i);
-				storeValueMin[3] = nodeE3.get(i);
-				storeValueMin[4] = nodeE4.get(i);
+				storeValueMin[1] = nodeParent.get(i);
+				storeValueMin[2] = nodeC1.get(i);
+				storeValueMin[3] = nodeC2.get(i);
+				storeValueMin[4] = nodeValue.get(i);
 			}
 		}
 
 		System.out.println("Node: :"+storeValueMax[0]+" MaxVal :"+storeValueMax[4]);
 		System.out.println("Node: :"+storeValueMin[0]+" MinVal :"+storeValueMin[4]);
-		System.out.println(nodeE4);
+		System.out.println(nodeValue);
 	}
 }
